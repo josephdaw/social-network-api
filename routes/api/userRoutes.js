5198ef3cd5ae6ff83acbcc0021d16b20e3ee077c
josephdaw <josephdaw@icloud.com>
2022-01-13 22:25:33 +1030

    get all users, create user, delete user

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,3 +1,26 @@
+const router = require('express').Router();
+const {
+  createUser,
+  getUsers,
+  getSingleUser,
+  updateUser,
+  deleteUser,
+} = require('../../controllers/userController.js');
+
 //  /api/users
+router.route('/')
+    .get(getUsers)
+    .post(createUser)
+    
+//  /api/users/:id
+router.route('/:userId')
+    .get(getSingleUser)
+    .put(updateUser)
+    .delete(deleteUser);
 
 //  /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId')
+    .get(getSingleUser);
+
+module.exports = router;
+
