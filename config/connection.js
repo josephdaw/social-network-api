c8e8bd66a6ce2207d4605ff8af506827c9d52010
josephdaw <josephdaw@icloud.com>
2022-01-18 15:08:44 +1030

    update DB name

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const { connect, connection } = require('mongoose');
 
-const connectionString = process.env.MONGODB_URI || 'mongodb://localhost:27017/studentsDB';
+const connectionString = process.env.MONGODB_URI || 'mongodb://localhost:27017/socialDB';
 
 connect(connectionString, {
   useNewUrlParser: true,
